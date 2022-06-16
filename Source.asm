0a49d56fe5729cd2b0d770eeaf20976857225bc7
nikcladis <nikcladis@gmail.com>
2022-06-16 04:11:34 +0300

    "correction 1"

--- Source.asm
+++ Source.asm
@@ -1,22 +1,20 @@
-TITLE Project
+TITLE Nikolaos Kladis Kountouris #ergasia arithmos 24
 INCLUDE Irvine32.inc
 
 
 .data
 
-A_poly DWORD 02Dh,0CCh,0E4h,0Fh
+A_poly BYTE 02Dh,0CCh,0E4h,0Fh
 m_power DWORD 3
 
-B_poly DWORD 0C7h,04Fh,72h
+B_poly BYTE 0C7h,04Fh,72h
 n_power DWORD 2
 
 P_poly DWORD ?
 p_power DWORD ?
 
 m1 BYTE "Polynomial A = ",0
-comma BYTE ", ",0
 m2 BYTE "Polynomial B = ",0
-close BYTE "}",0
 m3 BYTE "Polynomial P = ",0
 
 char1 BYTE " + ",0
@@ -25,23 +23,37 @@ char2 BYTE "x^",0
 
 .code
 
-;int *?ultiply ( int *A, int m, int *B, int n, int *P, int *p)
+;int multiply ( int *A, int m, int *B, int n, int *P, int *p)
 
 Multiply PROC
 
 push ebp
 mov ebp,esp
-pushad
+push edi
+
+mov eax, [ebp+20]
+add eax, [ebp+12]
+add eax, 1
+
+mov edi, [ebp+24]
+
+jmp cond
+
+lp:
+		mov [edi + esi*4], DWORD PTR 0
+		inc esi
+cond:
+		cmp esi, eax
+		jl lp
 
 
 
 
-
-popad
+pop edi
 mov esp,ebp
 pop ebp
 
-ret
+ret 24
 Multiply ENDP
 
 ;void PrintPoly(int P[], int n)
@@ -57,7 +69,8 @@ push edx
 mov esi, 0
 mov edi, [ebp+8]
 
-mov eax, [edi +esi*4]
+mov al, [edi]
+movzx eax, al
 call WriteInt
 
 mov edx, OFFSET char1
@@ -67,7 +80,7 @@ inc esi
 
 jmp cond
 lp:	
-		mov eax, [edi +esi*4]
+		mov al, [edi + esi]
 		call WriteInt
 		mov edx, OFFSET char2
 		call WriteString
@@ -82,13 +95,12 @@ cond:
 		cmp esi, [ebp+12]
 		jl lp
 
-mov eax, [edi +esi*4]
+mov al, [edi +esi]
 call WriteInt
 mov edx, OFFSET char2
 call WriteString
 mov eax, esi
 call WriteInt
-
 
 pop edx
 pop edi
@@ -105,7 +117,6 @@ main PROC
 push ebp
 mov ebp,esp
 sub esp,4
-mov [ebp-4], DWORD PTR 0; int p_power
 
 
 ;void PrintPoly(int P[], int n)
@@ -126,10 +137,9 @@ push n_power
 push OFFSET B_poly
 call PrintPoly
 
-;int *?ultiply ( int *A, int m, int *B, int n, int *P, int *p)
+;int multiply ( int *A, int m, int *B, int n, int *P, int *p)
 
-lea edi, [ebp-4]
-push edi
+push p_power
 push OFFSET P_poly
 push n_power
 push OFFSET B_poly
@@ -142,7 +152,7 @@ call Crlf
 mov edx, OFFSET m3
 call WriteString 
 
-push p_power
+push DWORD PTR [ebp-4]
 push OFFSET P_poly
 call PrintPoly
 
